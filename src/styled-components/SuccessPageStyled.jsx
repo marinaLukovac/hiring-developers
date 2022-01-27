fa05c1fc6236c63101ff4ab4097644d9639ca48a
Marina Lukovac <lukovacmarina@gmail.com>
2022-01-27 20:44:40 +0100

    Style SuccessPage

--- src/styled-components/SuccessPageStyled.jsx
+++ src/styled-components/SuccessPageStyled.jsx
@@ -16,9 +16,41 @@ const SuccessPageStyled = styled.article`
 		color: #808080;
 		font-size: 3rem;
 		font-weight: 300;
+		overflow-wrap: normal;
+		text-align: center;
 	}
 	& button {
-		height: 3rem;
+		height: 8rem;
+		width: 18rem;
+	}
+	@media all and (max-width: 1100px) and (max-height: 700px) {
+		& h2 {
+			font-size: 2.5rem;
+		}
+		& button {
+			height: 5rem;
+			width: 12rem;
+		}
+	}
+	@media all and (max-width: 550px) {
+		height: 80vh;
+		width: 80vw;
+		margin-top: 10vh;
+		& button {
+			height: 10rem;
+			width: 15rem;
+		}
+	}
+	@media all and (max-width: 420px) {
+		& button {
+			height: 10rem;
+			width: 15rem;
+		}
+	}
+		& button {
+			height: 10rem;
+			width: 20rem;
+		}
 	}
 `;
 export default SuccessPageStyled;
