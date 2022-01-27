ac300523a22b271b5c441d40590abcb3d2c13289
Marina Lukovac <lukovacmarina@gmail.com>
2022-01-27 16:58:17 +0100

    Remove logs and unnecessary code

--- src/isDevAvailable.js
+++ src/isDevAvailable.js
@@ -7,16 +7,13 @@ const checkIfDateIsOk = (startDate, endDate) => {
 	}
 	return [true, ''];
 };
-const checkDevAvailability = (now, contract, old) => {
-	//<current>,<object: start and endinging time>,<array of previous hiring times>
 
+const checkDevAvailability = (now, contract, old) => {
 	if (old.length === 0) return true;
 	for (let i = 0; i < old.length; i++) {
-		// i-1]<--c.start--[old.start--old.ending]--c.
 		let previous = null;
 		old[i - 1] ? (previous = old[i - 1].ending) : (previous = now);
-		//starting part,
-		console.log(now / 10000000, previous / 10000000, old[i].starting / 10000000, old[i].ending / 10000000, contract.starting / 10000000, contract.ending / 10000000);
+
 		if (previous < contract.starting && old[i].starting > contract.starting && old[i].starting < contract.ending) {
 			return false;
 		} else if ((old[i].starting <= contract.starting && old[i].ending >= contract.starting) || (old[i].starting <= contract.ending && old[i].ending >= contract.ending)) {
